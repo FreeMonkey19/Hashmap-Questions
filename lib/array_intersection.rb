7c3e08c5571a7dcd6cd6215fbf03991b45de9274
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-03-26 11:43:42 -0700

    file clean up after installing rubocop and some refactoring

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,20 +1,16 @@
-
-
 def intersection(list1, list2)
-  array = Array.new
-  nums= Hash.new
+  array = []
+  nums = {}
   i = 0
 
-  while list1.length > i 
+  while list1.length > i
     nums[list1[i]] = true
     i += 1
   end
 
   list2.each do |element|
     # the value at nums[element] is going to be nil or true
-    if nums[element]
-      array << element
-    end 
+    array << element if nums[element]
   end
-  return array
+  array
 end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,27 +1,25 @@
 def permutations?(string1, string2)
 
   def str_to_hash(string)
-      letters = Hash.new
-      i = 0
-
-      while string.length > i
-        if letters.key?(string[i])
-         letters[string[i]] += 1
-        else
-          letters[string[i]] = 1
-        end
-        i += 1
+    letters = {}
+    i = 0
+    while string.length > i
+      if letters.key?(string[i])
+        letters[string[i]] += 1
+      else
+        letters[string[i]] = 1
       end
-      letters
+      i += 1
+    end
+    letters
   end
 
-   string1_hash = str_to_hash(string1)
-   string2_hash = str_to_hash(string2)
-   
+  string1_hash = str_to_hash(string1)
+  string2_hash = str_to_hash(string2)
+
   if string1_hash != string2_hash
     false
   else
     true
   end
-
 end
