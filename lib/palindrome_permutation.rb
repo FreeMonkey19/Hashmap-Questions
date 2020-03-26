f34d9503a4899752c4006931febd51c5d43f58dc
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-03-26 12:06:32 -0700

    palindrome_permutataion? tests 1,2,3,4 passing

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,17 @@
-
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  return true if string.empty?
+
+  # create a new hash which will hold counts of chars
+ letters = Hash.new(0)  # iterate through the string, populate the hash
+ string.each_char do |char|
+   letters[char] += 1
+ end  # check the hash for chars that appear an odd number of times
+ odd_count = 0
+ letters.each do |_index, value|
+   # add to your odd char counter
+  if value % 2 == 0
+    odd_count += 1
+  end 
+ end  
+ odd_count > 1
 end
